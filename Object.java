9685a1c6e55d6fb05ec9883318c5ef29be05c0d3
Lesh <LeshDev@users.noreply.github.com>
2018-01-11 23:20:22 +0100

    Major Upload

---
+++ Object.java
@@ -0,0 +1,11 @@
+package de.lukas.schule.programm;
+
+public class Object {
+
+    public int zahl;
+    public static int[] munzen = {10,20,50,1,2};
+
+    public Object(int pZahl){
+        zahl = pZahl;
+    }
+}
